6fa06d07ee86f641fa070e75e53f21b3bf44b83c
Spade, Sean <sspade@axcess-financial.com>
2019-12-29 12:46:39 -0500

    chore: Reorders gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,31 @@ module.exports = {
     'MarkdownRemark.frontmatter.author': `AuthorJson`
   },
   plugins: [
+    // Parse all markdown files (each plugin add/parse some data into graphQL layer)
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 690,
+              backgroundColor: `#f7f0eb`
+            }
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-autolink-headers`
+        ]
+      }
+    },
+
+    // Parse all images files
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+
+    // Parse JSON files
+    `gatsby-transformer-json`,
     // Expose `/data` to graphQL layer
     {
       resolve: `gatsby-source-filesystem`,
@@ -37,12 +62,6 @@ module.exports = {
       }
     },
 
-    // Parse all images files
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-
-    // Parse JSON files
-    `gatsby-transformer-json`,
 
     // Add typescript stack into webpack
     `gatsby-plugin-typescript`,
